889cb86245fd34cb59a961320d5559f80d44bc67
Joseph Sevigny <joseph7e@login01.rcchpc>
2021-01-20 14:45:21 -0500

    initial addition of python script

---
+++ assess-sequence-availability.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# Author: Joseph Sevigny
+# Affiliation: Hubbard Center for Genome Studies, UNH
+# Purpose: Automate the retrievel of genome reports from NCBI, taxonomy data from SILVA, and taxonomy from the barcode of life database.
+
+
+
+
+
